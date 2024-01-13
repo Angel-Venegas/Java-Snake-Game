7e4410bdc1a9c53f5cadd57e42a1a9fba0339b9e
Angel Venegas <147934167+Angel-Venegas@users.noreply.github.com>
2024-01-13 13:46:08 -0800

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.snakegame.snake {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.snakegame.snake to javafx.fxml;
+    exports org.snakegame.snake;
+}
